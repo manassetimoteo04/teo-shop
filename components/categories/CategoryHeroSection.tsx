d3114ec768bcf27e70c10af169bd57a8c5c9ac94
manassetimoteo4 <manassetimoteo4@gmail.com>
2025-09-22 14:11:50 +0100

    Adding the images with

--- components/categories/CategoryHeroSection.tsx
+++ components/categories/CategoryHeroSection.tsx
@@ -1,13 +1,16 @@
 import Heading from "../ui/Heading";
+import HeroGridImages from "./HeroGridImages";
 
 export default function CategoryHeroSection() {
   return (
-    <div className="px-[6rem] flex-col   bg-gray-50 text-gray-500 h-[40rem] mt-[7rem] gap-[4rem] items-center flex justify-center">
+    <div className="px-[6rem] overflow-hidden relative flex-col   bg-gray-50 text-gray-500 h-[55rem] mt-[7rem] gap-[4rem] items-center flex justify-center">
       <Heading as="h1">Explorar Categorias</Heading>
       <span className="max-w-[40rem] flex items-center text-center">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
         molestiae itaque iusto cupiditate.
       </span>
+      <HeroGridImages position="left" />
+      <HeroGridImages position="right" />
     </div>
   );
 }
---
+++ components/categories/HeroGridImages.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import { MotionConfig, motion } from "framer-motion";
+
+const imgs = Array.from(
+  { length: 12 },
+  (_, i) => `/gallery/gallery-${i + 1}.jpg`
+);
+export default function HeroGridImages({
+  position,
+}: {
+  position: "left" | "right";
+}) {
+  const posit = {
+    right: "rotate-[50deg] right-0",
+    left: "-rotate-[50deg] left-0",
+  };
+  return (
+    <div>
+      <MotionConfig>
+        <motion.div
+          animate={{ opacity: 1 }}
+          className={`${posit[position]} absolute bottom-[-37rem]  grid  grid-cols-4 overflow-hidden  flex-col  gap-[0.4rem]`}
+        >
+          {imgs.map((img) => (
+            <motion.div
+              key={img}
+              className="w-[10rem] relative overflow-hidden bg-white p-[0.3rem] rounded-xl  h-[10rem]"
+            >
+              <img
+                draggable={false}
+                className="w-auto !h-full top-0 left-0 absolute object-contain"
+                src={img}
+                alt=""
+              />
+            </motion.div>
+          ))}{" "}
+          {imgs.map((img) => (
+            <motion.div
+              key={img}
+              className="w-[10rem] relative overflow-hidden bg-white p-[0.3rem] rounded-xl  h-[10rem]"
+            >
+              <img
+                draggable={false}
+                className="w-auto !h-full top-0 left-0 absolute object-contain"
+                src={img}
+                alt=""
+              />
+            </motion.div>
+          ))}
+          {imgs.map((img) => (
+            <motion.div
+              key={img}
+              className="w-[10rem] relative overflow-hidden bg-white p-[0.3rem] rounded-xl  h-[10rem]"
+            >
+              <img
+                draggable={false}
+                className="w-auto !h-full top-0 left-0 absolute object-contain"
+                src={img}
+                alt=""
+              />
+            </motion.div>
+          ))}
+        </motion.div>
+      </MotionConfig>
+    </div>
+  );
+}
